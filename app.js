456562d48e2683f55ec21c672394218d915905a7
unknown <M1020535@A2ML10965.mindtree.com>
2015-03-10 22:29:37 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+﻿var express = require('express');
+var path = require('path');
+var app = express();
+var bodyParser = require('body-parser');
+var jade = require('jade');
+
+//@@@@configure app
+//app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
+//__dirname is nodeJS variable gives the current working directory;
+app.set('views', path.join(__dirname, 'views'));
+
+
+//@@@@define middleware
+//get the user inputs from HTML page to NODEjs(from client to server without ajax)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//epxress.static is for serving static data like images,CSS and JS file,font files to client
+app.use(express.static(path.join(__dirname, 'bower_components')));
+
+//@@@@@define routes
+app.use(require('./router/router'));   
+//@@@@@starts server
+app.listen(8000, function () {
+    console.log('NODEJS Server is listening to 8000');
+});
---
+++ router/router.js
@@ -0,0 +1,51 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/*var toDoItems = [
+            { id: 1, desc: 'foo' },
+            { id: 2, desc: 'bor' },
+            { id: 3, desc: 'bor' },
+];
+router.get('/', function (req, res) {
+    //res.send('Hello Express');  
+    res.render('index', {
+        title: 'My App',
+        items: toDoItems
+    });
+});*/
+function User(name, email) {
+    this.name = name;
+    this.email = email;
+}
+
+var users = [
+    new User('tj', 'tj@vision-media.ca'),
+    new User('ciaran', 'ciaranj@gmail.com'),
+    new User('aaron', 'aaron.heckmann+github@gmail.com')
+];
+
+router.get('/', function (req, res) {
+    res.render('index', { users: users });
+});
+
+router.get('*', function (req, res) {
+    res.send(405, 'Method Not Allowwd');
+});
+
+router.post('/add', function (req, resp) {
+
+    var newItem = req.body.newItem;
+    console.log('form field', newItem);
+    toDoItems.push({
+        id: toDoItems.length + 1,
+        desc: newItem,
+    });
+
+    //once submit redirect to index or / page
+    resp.redirect("/");
+
+});
+
+
+
+module.exports = router;
